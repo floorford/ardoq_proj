3944eb5da6a417c7ace019b11d11a9ea7121c782
Flo Orford <flo.orford@hotmail.com>
2020-11-06 18:36:39 +0000

    basic styling of 404 page complete

--- src/components/FourOhFour.js
+++ src/components/FourOhFour.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FourOhFour = () => {
-  return <header>hi</header>;
+  return (
+    <section className='fourohfour'>
+      <div>
+        <h3>Uh oh!</h3>
+        <p>We're sorry but something went wrong!</p>
+        <p>
+          Feel like doing some math to release your frustrations? Head to the{" "}
+          <Link to='/calculator'>max product calculator</Link> page.
+        </p>
+        <p>
+          Maybe bikes make you smile - click <Link to='/oslobikes'>here</Link>{" "}
+          to find out about the ones in Oslo!
+        </p>
+        <p>
+          If you've totally had it, I recommend you click{" "}
+          <a href='https://giphy.com/explore/cat'>this.</a>
+        </p>
+      </div>
+      <div>
+        <img alt='shower cat' src='assets/cat.jpg' />
+      </div>
+    </section>
+  );
 };
 
 export default FourOhFour;
